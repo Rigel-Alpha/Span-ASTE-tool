ce9a80bc07fe0de6c11ce6673de6597da571a42d
LZR <1004586949@qq.com>
2024-07-24 02:44:23 +0800

    Make sure the env is correct

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Main Requirements
-pip install -r requirements.txt
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install torch==1.7.0+cu110 torchvision==0.8.1+cu110 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip uninstall dataclasses -y # Compatibility issue with Python > 3.6
 
 # Optional: Set up NLTK packages
